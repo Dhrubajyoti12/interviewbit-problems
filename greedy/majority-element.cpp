97b055d8e74fc3411aead94483c772480e74ed5e
norubai <orbay.altuntoprak@gmail.com>
2017-12-02 02:50:11 +0300

    Deleted the old files

--- greedy/majority-element.cpp
+++
@@ -1,9 +0,0 @@
-int Solution::majorityElement(const vector<int> &A) {
-	int count=0, value;
-	for(auto a : A){
-		if(value == a) 	++count;
-		else if(!count)	count=1, value=a;
-		else 			--count;
-	}
-	return value;
-}
